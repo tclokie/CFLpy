dc67e041afac71eea4de496db679a1548f2f67d4
Trevor Clokie <trevor.clokie@uwaterloo.ca>
2019-06-25 12:59:39 -0400

    Clean up code and remove unreachable variables

--- pda_to_cfg.py
+++ pda_to_cfg.py
@@ -1,81 +1,70 @@
+# PDA info
 states = {'-0', '-1', '-2', '+2', '+1(1)', '+1(10)', '+0(100)', '+0(10)', '+0(000)', 'END'}
-alphabet = {'0', '1'}
+#alphabet = {'0', '1'}
 stack_alphabet = {'Z', 'X'}
 start_state = '-0'
 start_stack = ['Z']
 delta = {
-    ('-0', '0', 'Z'): ('-0', 'Z'),
-    ('-0', '0', 'X'): ('-0', 'X'),
-    ('-0', '1', 'Z'): ('-1', 'Z'),
-    ('-0', '1', 'X'): ('-1', 'X'),
-    ('-1', '0', 'Z'): ('-0', 'XZ'),
-    ('-1', '0', 'X'): ('-0', 'XX'),
-    ('-1', '1', 'Z'): ('+2', 'Z'),
-    ('-1', '1', 'X'): ('-2', ''),
-    ('-2', '0', 'Z'): ('-1', 'Z'),
-    ('-2', '0', 'X'): ('-1', 'X'),
-    ('-2', '1', 'Z'): ('-2', 'Z'),
-    ('-2', '1', 'X'): ('-2', 'X'),
-    ('+2', '0', 'Z'): ('+1(10)', 'Z'),
-    ('+2', '0', 'X'): ('+1(10)', 'X'),
-    ('+2', '1', 'Z'): ('+2', 'Z'),
-    ('+2', '1', 'X'): ('+2', 'X'),
-    ('+1(10)', '0', 'Z'): ('-0', 'Z'),
-    ('+1(10)', '0', 'X'): ('+0(100)', ''),
-    ('+1(10)', '1', 'Z'): ('+2', 'XZ'),
-    ('+1(10)', '1', 'X'): ('+2', 'XX'),
-    ('+1(1)', '0', 'Z'): ('-0', 'Z'),
-    ('+1(1)', '0', 'X'): ('+0(10)', ''),
-    ('+1(1)', '1', 'Z'): ('+2', 'XZ'),
-    ('+1(1)', '1', 'X'): ('+2', 'XX'),
+    ('-0',      '0', 'Z'): ('-0',      'Z'),
+    ('-0',      '0', 'X'): ('-0',      'X'),
+    ('-0',      '1', 'Z'): ('-1',      'Z'),
+    ('-0',      '1', 'X'): ('-1',      'X'),
+    ('-1',      '0', 'Z'): ('-0',     'XZ'),
+    ('-1',      '0', 'X'): ('-0',     'XX'),
+    ('-1',      '1', 'Z'): ('+2',      'Z'),
+    ('-1',      '1', 'X'): ('-2',       ''),
+    ('-2',      '0', 'Z'): ('-1',      'Z'),
+    ('-2',      '0', 'X'): ('-1',      'X'),
+    ('-2',      '1', 'Z'): ('-2',      'Z'),
+    ('-2',      '1', 'X'): ('-2',      'X'),
+    ('+2',      '0', 'Z'): ('+1(10)',  'Z'),
+    ('+2',      '0', 'X'): ('+1(10)',  'X'),
+    ('+2',      '1', 'Z'): ('+2',      'Z'),
+    ('+2',      '1', 'X'): ('+2',      'X'),
+    ('+1(10)',  '0', 'Z'): ('-0',      'Z'),
+    ('+1(10)',  '0', 'X'): ('+0(100)',  ''),
+    ('+1(10)',  '1', 'Z'): ('+2',     'XZ'),
+    ('+1(10)',  '1', 'X'): ('+2',     'XX'),
+    ('+1(1)',   '0', 'Z'): ('-0',      'Z'),
+    ('+1(1)',   '0', 'X'): ('+0(10)',   ''),
+    ('+1(1)',   '1', 'Z'): ('+2',     'XZ'),
+    ('+1(1)',   '1', 'X'): ('+2',     'XX'),
     ('+0(000)', '0', 'Z'): ('+0(000)', 'Z'),
     ('+0(000)', '0', 'X'): ('+0(000)', 'X'),
-    ('+0(000)', '1', 'Z'): ('+1(1)', 'Z'),
-    ('+0(000)', '1', 'X'): ('+1(1)', 'X'),
-    ('+0(10)', '0', 'Z'): ('+0(100)', 'Z'),
-    ('+0(10)', '0', 'X'): ('+0(100)', 'X'),
-    ('+0(10)', '1', 'Z'): ('+1(1)', 'Z'),
-    ('+0(10)', '1', 'X'): ('+1(1)', 'X'),
+    ('+0(000)', '1', 'Z'): ('+1(1)',   'Z'),
+    ('+0(000)', '1', 'X'): ('+1(1)',   'X'),
+    ('+0(10)',  '0', 'Z'): ('+0(100)', 'Z'),
+    ('+0(10)',  '0', 'X'): ('+0(100)', 'X'),
+    ('+0(10)',  '1', 'Z'): ('+1(1)',   'Z'),
+    ('+0(10)',  '1', 'X'): ('+1(1)',   'X'),
     ('+0(100)', '0', 'Z'): ('+0(000)', 'Z'),
     ('+0(100)', '0', 'X'): ('+0(000)', 'X'),
-    ('+0(100)', '1', 'Z'): ('+1(1)', 'Z'),
-    ('+0(100)', '1', 'X'): ('+1(1)', 'X'),
-    ('+0(100)', '', 'Z'): ('END', ''),
-    ('+0(100)', '', 'X'): ('END', ''),
-    ('END', '', 'Z'): ('END', ''),
-    ('END', '', 'X'): ('END', '')
+    ('+0(100)', '1', 'Z'): ('+1(1)',   'Z'),
+    ('+0(100)', '1', 'X'): ('+1(1)',   'X'),
+    ('+0(100)',  '', 'Z'): ('END',      ''),
+    ('+0(100)',  '', 'X'): ('END',      ''),
+    ('END',      '', 'Z'): ('END',      ''),
+    ('END',      '', 'X'): ('END',      '')
 }
 
 
-'''
-def d(q, a, A): # a is '0' or '1', A is 'Z' or 'X'
-    if (q == '-0'):
-        if (a == '0'):
-            return (q, A)
-        else:
-            return ('-1', A)
-    elif (q == '-1'):
-        if (a == '0'):
-            return ('-0', 'X'+A)
-        else:
-            if (A == 'Z'):
-                return ('+2', A)
-            else:
-                return ('-2')
-'''
+
+# Make a CFG
 
 V = set()
 P = {}
+S = ('-0', 'Z', 'END')
 
+# Create V: set of variables
 for q in states:
     for A in stack_alphabet:
         for p in states:
-            V.add((q,A,p))
+            V.add((q, A, p))
 
-S = ('-0', 'Z', 'END')
-print(V.__contains__(S))
+# Sanity check
+print("Sanity check:", S in V)
 
-# Instantiate P
+# Instantiate P: set of productions
 for v in V:
     P[v] = []
 
@@ -83,36 +72,60 @@ for v in V:
 for q in states:
     for a in ['0', '1', '']:
         for A in stack_alphabet:
-            if (delta.__contains__((q, a, A))):
-                (q_1, B) = delta[(q,a,A)]
+            if ((q, a, A) in delta):
+                (q_1, B) = delta[(q, a, A)]
                 m = len(B)
                 if (m == 0):
-                    P[q,A,q_1].append([a])
+                    P[(q, A, q_1)].append([a])
                 elif (m == 1):
                     B_1 = B[0]
                     for q_2 in states:
-                        P[q,A,q_2].append([a, (q_1, B_1, q_2)])
+                        P[(q, A, q_2)].append([a, (q_1, B_1, q_2)])
                 elif (m == 2):
                     B_1 = B[0]
                     B_2 = B[1]
                     for q_2 in states:
                         for q_3 in states:
-                            P[q,A,q_3].append([a, (q_1, B_1, q_2), (q_2, B_2, q_3)])
+                            P[(q, A, q_3)].append([a, (q_1, B_1, q_2), (q_2, B_2, q_3)])
                 else:
                     print("SANITY CHECK FAILED")
                     break
 
+
+
+# using DFS, return set of variables reachable from parameter
+def find_reachable_vars (start):
+    seen = {start}
+    stack = [start]
+    while (len(stack) > 0):
+        current = stack.pop()
+        goes_to = P[current]
+        for var in goes_to:
+            for i in range (1, len(var)):
+                next = var[i]
+                if (not seen.__contains__(next)):
+                    seen.add(next)
+                    stack.append(next)
+    return seen
+
+
 # Eliminate dead production rules
 flag = True
 while (flag):
+    count = 0
+    for p in P.values():
+        count += len(p)
+    print(len(V), 'variables, and', count, 'production rules')
+    
     print("DELETING...")
     flag = False
-    to_remove_from_V = []
+    to_remove_from_V = set()
     to_remove_from_P = []
+    reachable_vars = find_reachable_vars(S)
     for v in V:
-        if (P[v] == []):
+        if (P[v] == [] or (not v in reachable_vars)):
             flag = True
-            to_remove_from_V.append(v)
+            to_remove_from_V.add(v)
     
     for v in to_remove_from_V:
         print('\t', v)
@@ -120,10 +133,11 @@ while (flag):
         del P[v]
         for (left, right) in P.items():
             for production in right:
-                if (production.__contains__(v)):
+                if (v in production):
                     print('\t\t', left, '\t', production)
                     to_remove_from_P.append((right, production))
     
     for (right, production) in to_remove_from_P:
         right.remove(production)
+
 
