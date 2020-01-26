03f5a27d5618eb06c98f3570bc2273838368b43c
Trevor Clokie <trevor.clokie@uwaterloo.ca>
2020-01-26 10:22:54 -0500

    Created function that prints PDA for Dot (GraphViz).

--- pda_to_cfg.py
+++ pda_to_cfg.py
@@ -38,7 +38,7 @@ def find_first_k_flimsies (k, limit): # Finds the k-flimsy integers in [1..limit
 
 
 
-
+# TODO: reverse stack direction
 class PDA:
     '''Pushdown Automata (accepting on empty stack only)'''
     def __init__ (self, states, alphabet, stack_alphabet, start_state, start_stack, transitions):
@@ -50,8 +50,8 @@ class PDA:
         self.transitions = transitions # here the top of the stack is on the RIGHT of the stack_after string
         # TODO: verify that there are no infinite epsilon loops
 
+    ''' Converts PDA into CFG using Hopcroft & Ullman "triple" production'''
     def to_CFG (self):
-        # return CFG(self)
         cfg = CFG()
         cfg.init_variables(self)
         cfg.add_to_alphabet(self.alphabet)
@@ -60,6 +60,8 @@ class PDA:
         cfg.simplify()
         return cfg
 
+    # Simulates PDA on an input x; determines whether or not PDA accepts
+    # TODO: Doesn't work?
     def simulate (self, x): # boolean: does this PDA accept input x?
         return self._simulate_(self.start_state, self.start_stack[0], x)
 
@@ -99,10 +101,45 @@ class PDA:
         output.extend(temp)
         return output
 
+    def simplify (self):
+        # remove unreachable states
+        # remove unproductive states
+        # verify that there are no infinite epsilon loops?
+        return
 
+    '''Outputs PDA as a .gv file, to be printed by dot'''
     def output_gv (self):
-        # TODO: return valid output for .gv dot file
-        return ""
+        output = ['digraph G {']
+        output.append('\t"" [shape=none];') # Transition to start state
+        for state in self.states:
+            output.append('\t"' + str(state) + '";')
+
+        output.append('\t') # a space for the sake of readability
+
+        output.append('\t"" -> "' + str(self.start_state) + '" [label="&epsilon;, &epsilon;/' + str(self.start_stack[0]) + '"];')
+
+        for from_state in self.states:
+            for to_state in self.states:
+                conditions = []
+                for read_symbol in self.alphabet:
+                    for stack_top in self.stack_alphabet:
+                        if (from_state, read_symbol, stack_top) in self.transitions:
+                            for (a,new_stack_top) in self.transitions[(from_state, read_symbol, stack_top)]:
+                                if a == to_state:
+                                    conditions.append((read_symbol, stack_top, new_stack_top))
+                if len(conditions) > 0:
+                    build_string = '\t"' + str(from_state) + '" -> "' + str(to_state) + '" [label="'
+                    for (read_symbol, stack_top, new_stack_top) in conditions:
+                        if read_symbol == "":
+                            read_symbol = "&epsilon;"
+                        if new_stack_top == "":
+                            new_stack_top = "&epsilon;"
+                        build_string += str(read_symbol)+', '+str(stack_top)+'/'+str(new_stack_top)+'\\n'
+                    build_string = build_string[:-2] + '"];' # strip off the last \n and replace with "];
+                    output.append(build_string)
+
+        output.append('}')
+        return output
 
 
 
@@ -856,25 +893,13 @@ def create_k_equal_PDA (k): # Only works for k=3 so far
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 if (len(sys.argv) > 1) and (sys.argv):
     k = int(sys.argv[1])
     # fname = "./maple_files/" + ("{:02d}".format(k)) + "_cfg.maple"
     fname = "./maple_files/" + ("{:02d}".format(k)) + "_equal.maple"
 
-    pda = create_flimsy_PDA(k)
-    #pda = create_k_equal_PDA(k)
+    # pda = create_flimsy_PDA(k)
+    pda = create_k_equal_PDA(k)
     # print_array(pda.to_string_array())
     cfg = pda.to_CFG()
     # print_array(cfg.to_string_array())
@@ -884,12 +909,3 @@ if (len(sys.argv) > 1) and (sys.argv):
     for line in output:
         f.write(line + '\n')
     f.close()
-
-else:
-    pda = create_Jeffs_even_palindrome_PDA()
-    # pda = create_palindrome_PDA()
-    # pda = create_dyck_PDA()
-    print_array(pda.to_string_array())
-    print()
-    cfg = pda.to_CFG()
-    print_array(cfg.to_string_array())
